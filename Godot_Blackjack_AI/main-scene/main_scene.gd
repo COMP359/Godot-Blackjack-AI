a99f2f60c366cd3ef48b1cb68e3dfa4fe2db723e
Braedon McRae <braedonmcrae98@gmail.com>
2024-02-13 17:59:15 -0800

    Added card textures; simple scene switch
    - Card PNG files now available in `images/cards`
    - Clicking "Play" now takes you to gameplay scene

--- Godot_Blackjack_AI/main-scene/main_scene.gd
+++ Godot_Blackjack_AI/main-scene/main_scene.gd
@@ -1,12 +1,10 @@
 extends Control
 
 func _on_start_button_pressed():
-	pass # Replace with function body.
-
+	get_tree().change_scene_to_file("res://gameplay-scene/game.tscn")
 
 func _on_options_pressed():
 	pass # Replace with function body.
-
 
 func _on_close_game_pressed():
 	get_tree().quit()
