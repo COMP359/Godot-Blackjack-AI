2a81a34593616dafd99553962fcf6534439c05b4
Braedon McRae <braedonmcrae98@gmail.com>
2024-02-13 17:17:09 -0800

    Title screen skeleton
    Added menu with buttons and basic script to go with them

---
+++ Godot_Blackjack_AI/main-scene/main_scene.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_start_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+
+func _on_close_game_pressed():
+	get_tree().quit()
